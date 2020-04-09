f5823476bdcc89fe1f0bcf44c091b2f6ddb4660c
Julian1313 <jochoa1011@gmail.com>
2020-04-09 13:38:29 -0700

    organizes jquery

--- jquery.js
+++ jquery.js
@@ -1,26 +1,10 @@
 $(document).on("click", ".start-button", function() {
-    $(".start-screen").hide();
-    $(".pre-level-one").show();
-  });
-  
-  $(document).on("click", ".pre1-button", function() {
-    $(".pre-level-one").hide(1000);
+  $(".start-screen").hide();
+  $(".pre-level-one").show();
+ setTimeout(function(){
+    $(".pre-level-one").hide();
     $(".level-one").show();
-  });
-  
-  $(document).ready(function(e) {
-    var width = "+=" + $(document).width();
     $("#animate").animate({
-    left: width
-  }, 5000, function() {
-  });
-  });
-  
-  /*const startButton = document.querySelector(".start-button")
-  const startScreen = document.querySelector(".start-screen")
-  const preLevelOne = document.querySelector(".pre-level-one")
-  
-  startButton.addEventListener("click", function(){
-    startScreen.style.display = "none"
-    preLevelOne.style.display = "flex"
-  })*/
+    left: "-400px" }, 3000);
+   }, 6000);
+});
