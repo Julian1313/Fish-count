6115df7e64879d83111e680bfea8ed3ba8a89265
Julian1313 <jochoa1011@gmail.com>
2020-04-09 14:44:06 -0700

    removes content

--- fish-count.js
+++ fish-count.js
@@ -1,16 +1,16 @@
 //setTimeout(function() { alert("my message"); }, 5000);
 
 
-/*function fade_out() {
-  $(".instructions-one").fadeOut().empty();
-}
+// function fade_out() {
+//   $(".instructions-one").fadeOut().empty();
+// }
 
-/*setTimeout2(fade_out, 5000);
+// setTimeout2(fade_out, 5000);
 
-function fade_out() {
-  $(".instructions-img1").fadeOut().empty();
-}
-*/
+// function fade_out() {
+//   $(".instructions-img1").fadeOut().empty();
+// }
+
 // let fade_out = function() {
 //     $(".instructions-one").fadeOut().empty();
 //     $(".instructions-img1").fadeOut().empty();
