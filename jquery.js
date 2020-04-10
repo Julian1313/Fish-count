cda9df00de92a22439c77f80f099e3c3440394bd
Julian1313 <jochoa1011@gmail.com>
2020-04-10 01:40:42 -0700

    makes animation smoother

--- jquery.js
+++ jquery.js
@@ -8,22 +8,22 @@ $(document).on("click", ".start-button", function() {
     $("#animate").animate({
     left: "-1500px" }, 9000);
     $("#animate1").animate({
-    left: "-1500px" }, 8000);
-    $("#animate2").animate({
-    left: "-1500px" }, 7000);
-    $("#animate3").animate({
     left: "-1500px" }, 6000);
+    $("#animate2").animate({
+    left: "-1500px" }, 5000);
+    $("#animate3").animate({
+    left: "-1500px" }, 8000);
     $("#animate4").animate({
     left: "-1500px" }, 5000);
 
     $("#animate5").animate({
-    left: "-1500px" }, 5000);
+    left: "-1500px" }, 8000);
     $("#animate6").animate({
-    left: "-1500px" }, 5000);
+    left: "-1500px" }, 6000);
     $("#animate7").animate({
-    left: "-1500px" }, 5000);
+    left: "-1500px" }, 9000);
     $("#animate8").animate({
-    left: "-1500px" }, 5000);
+    left: "-1500px" }, 9000);
 
     $("#animate9").animate({
     left: "-1500px" }, 9000);
@@ -36,9 +36,9 @@ $(document).on("click", ".start-button", function() {
     $("#animate13").animate({
     left: "-1500px" }, 5000);
     $("#animate14").animate({
-    left: "-1500px" }, 6000);
+    left: "-1500px" }, 3000);
     $("#animate15").animate({
-    left: "-1500px" }, 5000);
+    left: "-1500px" }, 4000);
    }, 6000);
    setTimeout(function() {
      prompt("How many fish did you count?")
@@ -63,18 +63,18 @@ $(document).on("click", ".start-button2", function() {
         $("#animate18").animate({
         left: "-1500px" }, 7000);
         $("#animate19").animate({
-        left: "-1500px" }, 6000);
+        left: "-1500px" }, 5000);
         $("#animate20").animate({
         left: "-1500px" }, 5000);
     
         $("#animate21").animate({
         left: "-1500px" }, 5000);
         $("#animate22").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 4000);
         $("#animate23").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 8000);
         $("#animate24").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 9000);
     
         $("#animate25").animate({
         left: "-1500px" }, 9000);
@@ -87,9 +87,9 @@ $(document).on("click", ".start-button2", function() {
         $("#animate29").animate({
         left: "-1500px" }, 5000);
         $("#animate30").animate({
-        left: "-1500px" }, 6000);
+        left: "-1500px" }, 3000);
         $("#animate31").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 4000);
        }, 6000);
        setTimeout(function() {
         prompt("How many fish did you count?")
@@ -108,9 +108,9 @@ $(document).on("click", ".start-button3", function() {
         $(".level-three").show();
     
         $("#animate32").animate({
-        left: "-1500px" }, 9000);
+        left: "-1500px" }, 3000);
         $("#animate33").animate({
-        left: "-1500px" }, 8000);
+        left: "-1500px" }, 4000);
         $("#animate34").animate({
         left: "-1500px" }, 7000);
         $("#animate35").animate({
@@ -119,13 +119,13 @@ $(document).on("click", ".start-button3", function() {
         left: "-1500px" }, 5000);
     
         $("#animate37").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 7000);
         $("#animate38").animate({
         left: "-1500px" }, 5000);
         $("#animate39").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 4000);
         $("#animate40").animate({
-        left: "-1500px" }, 5000);
+        left: "-1500px" }, 8000);
     
         $("#animate41").animate({
         left: "-1500px" }, 9000);
